399c2854ccdc2f0959e788a1f33faa2cfab75ffd
Lich2013 <longzy2013@gmail.com>
2017-08-19 21:57:33 +0800

    aria2 add uri ok

--- callerPlugin/torrent.go
+++ callerPlugin/torrent.go
@@ -1,21 +1,25 @@
 package callerPlugin
 
 import (
-	"os/exec"
 	"fmt"
 	"raspberry-client/g"
 )
 
 
-func CallAria2c(x g.Task)  {
-	cmd := exec.Command("ls")
-	output, err := cmd.Output()
-	fmt.Println(err, string(output))
+
+func CallAria2c(x g.Task) {
+	err := g.RPC.Ping()
 	if err != nil {
 		g.LogFatal <- fmt.Sprintf("task %+v faild, error is %s", x, err.Error())
 		return
 	}
-	g.LogInfo <- fmt.Sprintf("task %+v success", x)
+	uri := []string{x.TaskDetail}
+	ret, err := g.RPC.AddUri(uri)
+	if err != nil {
+		g.LogFatal <- fmt.Sprintf("task %+v faild, error is %s", x, err.Error())
+		return
+	}
+	g.LogInfo <- fmt.Sprintf("task %+v success, task id is %s", x, ret)
 	fmt.Println(x)
 
 }
